257064d93bd187bb93a103a75e7c442a8b9eab20
aditya vatsa <adityavatsa08@gmail.com>
2021-12-18 22:19:47 +0530

    book routes setup done with database

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const { booksRouter } = require("./routes/books");
 
 const app = express();
 
@@ -7,7 +9,24 @@ app.use(express.json());
 
 // constants
 const PORT = 3000;
+const API_URL = "api";
+const MONGO_URI =
+    "mongodb+srv://itscoolvatsa:mongodbpassword@cluster0.cikjf.mongodb.net/webstore-1?retryWrites=true&w=majority";
 
+// database connection
+mongoose
+    .connect(MONGO_URI)
+    .then(() => {
+        console.log("Database Connected");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+// routes
+app.use(`/${API_URL}/books`, booksRouter);
+
+// server setup
 app.listen(PORT, () => {
     console.log(`server is running on PORT ${PORT}`);
 });
--- routes/books.js
+++ routes/books.js
@@ -1,9 +1,80 @@
 const express = require("express");
+const { Book } = require("../models/book");
 
 const router = express.Router();
 
 // GET all books
-router.get("/", (req, res) => {});
+router.get("/", async (req, res) => {
+    const books = await Book.find({});
+
+    res.status(200).send(books);
+});
+
+// GET a book by ID
+router.get("/:id", async (req, res) => {
+    const id = req.params.id;
+
+    const book = await Book.findById(id).catch((err) => {
+        console.log("Invalid ID");
+    });
+
+    if (!book) {
+        res.status(400).send("Can't fetch the book details");
+    }
+
+    res.status(200).send(book);
+});
+
+// Add a book
+router.post("/", async (req, res) => {
+    const { name, author, description, price } = req.body;
+
+    const book = new Book({ name, author, description, price });
+
+    await book.save();
+
+    res.status(201).send(book);
+});
+
+// Update a book
+router.put("/:id", async (req, res) => {
+    const id = req.params.id;
+    const { name, author, description, price } = req.body;
+
+    const book = await Book.findById(id).catch((err) => {
+        console.log("Invalid ID");
+    });
+
+    if (!book) {
+        res.status(400).send("Invalid ID");
+    }
+
+    const updatedBook = await Book.findByIdAndUpdate(id, {
+        name,
+        author,
+        description,
+        price,
+    });
+
+    res.status(200).send(updatedBook);
+});
+
+// Delete a book
+router.delete("/:id", async (req, res) => {
+    const id = req.params.id;
+
+    const book = await Book.findById(id).catch((err) => {
+        console.log("Invalid ID");
+    });
+
+    if (!book) {
+        res.status(400).send("Invalid ID");
+    }
+
+    const updatedBook = await Book.findByIdAndDelete(id);
+
+    res.status(200).send("Data deleted");
+});
 
 module.exports = {
     booksRouter: router,
